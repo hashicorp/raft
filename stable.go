38cfb5c58d57e079cd14ce0b997c88dbe505449c
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-05 17:00:31 -0800

    simplify the stable storage interface, push to higher level

--- stable.go
+++ stable.go
@@ -3,19 +3,9 @@ package raft
 // StableStore is used to provide stable storage
 // of key configurations to ensure safety.
 type StableStore interface {
-	// Returns the current term
-	CurrentTerm() (uint64, error)
+	Set(key []byte, val []byte) error
+	Get(key []byte) ([]byte, error)
 
-	// Returns the candidate we voted for this term
-	VotedFor() (string, error)
-
-	// Sets the current term. Clears the current vote.
-	SetCurrentTerm(uint64) error
-
-	// Sets a candidate vote for the current term
-	SetVote(string) error
-
-	// Returns our candidate ID. This should be unique
-	// and constant across runs
-	CandidateID() (string, error)
+	SetUint64(key []byte, val uint64) error
+	GetUint64(key []byte) (uint64, error)
 }
