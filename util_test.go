5fcfbf3998ef340504fd2b5ababb9049d0760d7d
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-06 14:39:08 -0800

    Adding UUID test

--- util_test.go
+++ util_test.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"regexp"
 	"testing"
 	"time"
 )
@@ -29,5 +30,21 @@ func TestMin(t *testing.T) {
 	}
 	if min(1, 2) != 1 {
 		t.Fatalf("bad min")
+	}
+}
+
+func TestGenerateUUID(t *testing.T) {
+	prev := generateUUID()
+	for i := 0; i < 100; i++ {
+		id := generateUUID()
+		if prev == id {
+			t.Fatalf("Should get a new ID!")
+		}
+
+		matched, err := regexp.MatchString(
+			"[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}", id)
+		if !matched || err != nil {
+			t.Fatalf("expected match %s %v %s", id, matched, err)
+		}
 	}
 }
