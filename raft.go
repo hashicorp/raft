ad54864bf18c4e10e97cc6f915d35241a60f4d53
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-06 10:14:24 -0800

    Refactor to share follower code with candidate

--- raft.go
+++ raft.go
@@ -118,9 +118,9 @@ func (r *Raft) runFollower() {
 			// Handle the command
 			switch cmd := rpc.Command.(type) {
 			case *AppendEntriesRequest:
-				r.followerAppendEntries(rpc, cmd)
+				r.appendEntries(rpc, cmd)
 			case *RequestVoteRequest:
-				r.followerRequestVote(rpc, cmd)
+				r.requestVote(rpc, cmd)
 			default:
 				log.Printf("[ERR] Follower state, got unexpected command: %#v",
 					rpc.Command)
@@ -145,11 +145,19 @@ func (r *Raft) runCandidate() {
 		select {
 		case rpc := <-ch:
 			// Handle the command
-			switch rpc.Command.(type) {
+			transition := false
+			switch cmd := rpc.Command.(type) {
+			case *AppendEntriesRequest:
+				transition = r.appendEntries(rpc, cmd)
+			case *RequestVoteRequest:
+				transition = r.requestVote(rpc, cmd)
 			default:
 				log.Printf("[ERR] Candidate state, got unexpected command: %#v",
 					rpc.Command)
 				rpc.Respond(nil, fmt.Errorf("Unexpected command"))
+			}
+			if transition {
+				return
 			}
 
 		case <-randomTimeout(r.conf.ElectionTimeout):
@@ -183,4 +191,157 @@ func (r *Raft) Shutdown() {
 		close(r.shutdownCh)
 		r.shutdownCh = nil
 	}
+}
+
+// appendEntries is invoked when we get an append entries RPC call
+// Returns true if we transition to a Follower
+func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool) {
+	// Setup a response
+	resp := &AppendEntriesResponse{
+		Term:    r.currentTerm,
+		Success: false,
+	}
+	var err error
+	defer rpc.Respond(resp, err)
+
+	// Ignore an older term
+	if a.Term < r.currentTerm {
+		return
+	}
+
+	// Increase the term if we see a newer one
+	if a.Term > r.currentTerm {
+		r.currentTerm = a.Term
+		resp.Term = a.Term
+
+		// Ensure transition to follower
+		transition = true
+		r.state = Follower
+	}
+
+	// Verify the last log entry
+	var prevLog Log
+	if err := r.logs.GetLog(a.PrevLogEntry, &prevLog); err != nil {
+		log.Printf("[WARN] Failed to get previous log: %d %v",
+			a.PrevLogEntry, err)
+		return
+	}
+	if a.PrevLogTerm != prevLog.Term {
+		log.Printf("[WARN] Previous log term mis-match: ours: %d remote: %d",
+			prevLog.Term, a.PrevLogTerm)
+		return
+	}
+
+	// Add all the entries
+	for _, entry := range a.Entries {
+		// Delete any conflicting entries
+		if entry.Index <= r.lastLog {
+			log.Printf("[WARN] Clearing log suffix from %d to %d", entry.Index, r.lastLog)
+			if err := r.logs.DeleteRange(entry.Index, r.lastLog); err != nil {
+				log.Printf("[ERR] Failed to clear log suffix: %v", err)
+				return
+			}
+		}
+
+		// Append the entry
+		if err := r.logs.StoreLog(entry); err != nil {
+			log.Printf("[ERR] Failed to append to log: %v", err)
+			return
+		}
+
+		// Update the lastLog
+		r.lastLog = entry.Index
+	}
+
+	// Update the commit index
+	if a.LeaderCommitIndex > r.commitIndex {
+		r.commitIndex = min(a.LeaderCommitIndex, r.lastLog)
+
+		// TODO: Trigger applying logs locally!
+	}
+
+	// Everything went well, set success
+	resp.Success = true
+	return
+}
+
+// requestVote is invoked when we get an request vote RPC call
+// Returns true if we transition to a Follower
+func (r *Raft) requestVote(rpc RPC, req *RequestVoteRequest) (transition bool) {
+	// Setup a response
+	resp := &RequestVoteResponse{
+		Term:    r.currentTerm,
+		Granted: false,
+	}
+	var err error
+	defer rpc.Respond(resp, err)
+
+	// Ignore an older term
+	if req.Term < r.currentTerm {
+		return
+	}
+
+	// Increase the term if we see a newer one
+	if req.Term > r.currentTerm {
+		r.currentTerm = req.Term
+		resp.Term = req.Term
+
+		// Ensure transition to follower
+		transition = true
+		r.state = Follower
+	}
+
+	// Check if we have voted yet
+	lastVoteTerm, err := r.stable.GetUint64(keyLastVoteTerm)
+	if err != nil && err.Error() != "not found" {
+		log.Printf("[ERR] Failed to get last vote term: %v", err)
+		return
+	}
+	lastVoteCandBytes, err := r.stable.Get(keyLastVoteCand)
+	if err != nil && err.Error() != "not found" {
+		log.Printf("[ERR] Failed to get last vote candidate: %v", err)
+		return
+	}
+
+	// Check if we've voted in this election before
+	if lastVoteTerm == req.Term && lastVoteCandBytes != nil {
+		log.Printf("[INFO] Duplicate RequestVote for same term: %d", req.Term)
+		if string(lastVoteCandBytes) == req.CandidateId {
+			log.Printf("[WARN] Duplicate RequestVote from candidate: %s", req.CandidateId)
+			resp.Granted = true
+		}
+		return
+	}
+
+	// Reject if their term is older
+	if r.lastLog > 0 {
+		var lastLog Log
+		if err := r.logs.GetLog(r.lastLog, &lastLog); err != nil {
+			log.Printf("[ERR] Failed to get last log: %d %v",
+				r.lastLog, err)
+			return
+		}
+		if lastLog.Term > req.LastLogTerm {
+			log.Printf("[WARN] Rejecting vote since our last term is greater")
+			return
+		}
+
+		if lastLog.Index > req.LastLogIndex {
+			log.Printf("[WARN] Rejecting vote since our last index is greater")
+			return
+		}
+	}
+
+	// Seems we should grant a vote
+	if err := r.stable.SetUint64(keyLastVoteTerm, req.Term); err != nil {
+		log.Printf("[ERR] Failed to persist last vote term: %v", err)
+		return
+	}
+	if err := r.stable.Set(keyLastVoteCand, []byte(req.CandidateId)); err != nil {
+		log.Printf("[ERR] Failed to persist last vote candidate: %v", err)
+		return
+	}
+
+	resp.Granted = true
+	return
 }
