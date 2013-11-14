d74273a4a7af859f2c5211049cb06bc2ec5e4c4a
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-14 15:02:29 -0800

    Adding snapshot related configs

--- config.go
+++ config.go
@@ -29,14 +29,32 @@ type Config struct {
 	// If ShutdownOnRemove is is set, we additional shutdown Raft. Otherwise,
 	// we can become a leader of a cluster containing only this node.
 	ShutdownOnRemove bool
+
+	// TrailingLogs controls how many logs we leave after a snapshot. This is
+	// used so that we can quickly replay logs on a follower instead of being
+	// forced to send an entire snapshot.
+	TrailingLogs int
+
+	// SnapshotInterval controls how often we check if we should perform a snapshot.
+	// We randomly stagger between this value and 2x this value to avoid the entire
+	// cluster from performing a snapshot at once
+	SnapshotInterval time.Duration
+
+	// SnapshotThreshold controls how many outstanding logs there must be before
+	// we perform a snapshot. This is to prevent excessive snapshots when we can
+	// just replay a small set of logs.
+	SnapshotThreshold int
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		HeartbeatTimeout: 200 * time.Millisecond,
-		ElectionTimeout:  250 * time.Millisecond,
-		CommitTimeout:    10 * time.Millisecond,
-		MaxAppendEntries: 32,
-		ShutdownOnRemove: true,
+		HeartbeatTimeout:  200 * time.Millisecond,
+		ElectionTimeout:   250 * time.Millisecond,
+		CommitTimeout:     10 * time.Millisecond,
+		MaxAppendEntries:  32,
+		ShutdownOnRemove:  true,
+		TrailingLogs:      1024,
+		SnapshotInterval:  5 * time.Minute,
+		SnapshotThreshold: 8192,
 	}
 }
