8b26888a2457af3ff0d190c38b5aa31ebeb6e713
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-14 12:22:53 -0800

    Adding basic inmem transport tests

---
+++ inmem_transport_test.go
@@ -0,0 +1,30 @@
+package raft
+
+import (
+	"net"
+	"testing"
+)
+
+func TestInmemAddrImpl(t *testing.T) {
+	var inm interface{} = NewInmemAddr()
+	if _, ok := inm.(net.Addr); !ok {
+		t.Fatalf("InmemAddr is not a net.Addr")
+	}
+}
+
+func TestInmemAddr(t *testing.T) {
+	inm := NewInmemAddr()
+	if inm.Network() != "inmem" {
+		t.Fatalf("bad network")
+	}
+	if inm.String() != inm.Id {
+		t.Fatalf("bad string")
+	}
+}
+
+func TestInmemTransportImpl(t *testing.T) {
+	var inm interface{} = &InmemTransport{}
+	if _, ok := inm.(Transport); !ok {
+		t.Fatalf("InmemTransport is not a Transport")
+	}
+}
