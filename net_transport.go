e1f05ba7cab2275fefcfbbe761b7c4ab7d75e255
Armon Dadgar <armon.dadgar@gmail.com>
2013-11-18 11:19:27 -0800

    Decoding of requests

--- net_transport.go
+++ net_transport.go
@@ -1,18 +1,44 @@
 package raft
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/ugorji/go/codec"
 	"io"
 	"log"
 	"net"
 	"sync"
 )
 
-// NetworkTransport provides a network based transport that can be
-// used to communicate with Raft on remote machines. It requires
-// an underlying layer to provide a stream abstraction, which can
-// be simple TCP, TLS, or something more advanced. Underlying addresses
-// must be castable to TCPAddr
+const (
+	rpcAppendEntries uint8 = iota
+	rpcRequestVote
+	rpcInstallSnapshot
+)
+
+var (
+	TransportShutdown = fmt.Errorf("transport shutdown")
+)
+
+/*
+
+NetworkTransport provides a network based transport that can be
+used to communicate with Raft on remote machines. It requires
+an underlying layer to provide a stream abstraction, which can
+be simple TCP, TLS, etc. Underlying addresses must be castable to TCPAddr
+
+This transport is very simple and lightweight. Each RPC request is
+framed by sending a byte that indicates the message type, followed
+by the MsgPack encoded request.
+
+The response is an error string followed by the response object,
+both are encoded using MsgPack.
+
+InstallSnapshot is special, in that after the RPC request we stream
+the entire state. That socket is not re-used as the connection state
+is not known if there is an error.
+
+*/
 type NetworkTransport struct {
 	connPool     map[string][]net.Conn
 	connPoolLock sync.Mutex
@@ -109,6 +135,92 @@ func (n *NetworkTransport) listen() {
 // handleConn is used to handle an inbound connection for its lifespan
 func (n *NetworkTransport) handleConn(conn net.Conn) {
 	defer conn.Close()
+	r := bufio.NewReader(conn)
+	w := bufio.NewWriter(conn)
+	dec := codec.NewDecoder(r, &codec.MsgpackHandle{})
+	enc := codec.NewEncoder(w, &codec.MsgpackHandle{})
 
-	// TODO:
+	for {
+		if err := n.handleCommand(r, dec, enc); err != nil {
+			if err != io.EOF {
+				log.Printf("[ERR] Failed to decode incoming command: %v", err)
+			}
+			return
+		}
+		if err := w.Flush(); err != nil {
+			log.Printf("[ERR] Failed to flush response: %v", err)
+			return
+		}
+	}
+}
+
+// handleCommand is used to decode and dispatch a single command
+func (n *NetworkTransport) handleCommand(r *bufio.Reader, dec *codec.Decoder, enc *codec.Encoder) error {
+	// Get the rpc type
+	rpcType, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	// Create the RPC object
+	respCh := make(chan RPCResponse)
+	rpc := RPC{
+		RespChan: respCh,
+	}
+
+	// Decode the command
+	switch rpcType {
+	case rpcAppendEntries:
+		var req AppendEntriesRequest
+		if err := dec.Decode(&req); err != nil {
+			return err
+		}
+		rpc.Command = &req
+
+	case rpcRequestVote:
+		var req RequestVoteRequest
+		if err := dec.Decode(&req); err != nil {
+			return err
+		}
+		rpc.Command = &req
+
+	case rpcInstallSnapshot:
+		var req InstallSnapshotRequest
+		if err := dec.Decode(&req); err != nil {
+			return err
+		}
+		rpc.Command = &req
+		rpc.Reader = io.LimitReader(r, req.Size)
+
+	default:
+		return fmt.Errorf("unknown rpc type %d", rpcType)
+	}
+
+	// Dispatch the RPC
+	select {
+	case n.consumeCh <- rpc:
+	case <-n.shutdownCh:
+		return TransportShutdown
+	}
+
+	// Wait for response
+	select {
+	case resp := <-respCh:
+		// Send the error first
+		respErr := ""
+		if resp.Error != nil {
+			respErr = resp.Error.Error()
+		}
+		if err := enc.Encode(respErr); err != nil {
+			return err
+		}
+
+		// Send the response
+		if err := enc.Encode(resp.Response); err != nil {
+			return err
+		}
+	case <-n.shutdownCh:
+		return TransportShutdown
+	}
+	return nil
 }
