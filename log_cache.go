9bf7eba137fb6668cc0044e56501a634e7ddc9d0
Armon Dadgar <armon.dadgar@gmail.com>
2015-01-14 15:27:53 -0800

    Minor cleanups

--- log_cache.go
+++ log_cache.go
@@ -4,20 +4,26 @@ import (
 	"sync"
 )
 
-// LogCache wraps a logstore with a ring buffer providing fast access to the
-// last n raft log entries.
+// LogCache wraps any LogStore implementation to provide an
+// in-memory ring buffer. This is used to cache access to
+// the recently written entries. For implementations that do not
+// cache themselves, this can provide a substantial boost by
+// avoiding disk I/O on recent entries.
 type LogCache struct {
-	store      LogStore
+	store LogStore
+
 	cache      []*Log
 	current    int
-	lastlogidx uint64
+	lastLogIdx uint64
 	l          sync.RWMutex
 }
 
-func NewLogCache(capacity int, logstore LogStore) *LogCache {
+// NewLogCache is used to create a new LogCache with the
+// given capacity and backend store.
+func NewLogCache(capacity int, store LogStore) *LogCache {
 	return &LogCache{
+		store: store,
 		cache: make([]*Log, capacity),
-		store: logstore,
 	}
 }
 
@@ -26,9 +32,9 @@ func (c *LogCache) getLogFromCache(logidx uint64) (*Log, bool) {
 	defer c.l.RUnlock()
 
 	// 'last' is the index of the element we cached last,
-	// its raft log index is 'lastlogidx'
+	// its raft log index is 'lastLogIdx'
 	last := (c.current - 1)
-	m := last - int(c.lastlogidx-logidx)
+	m := last - int(c.lastLogIdx-logidx)
 
 	// See https://golang.org/issue/448 for why (m % n) is not enough.
 	n := len(c.cache)
@@ -49,17 +55,17 @@ func (c *LogCache) cacheLogs(logs []*Log) {
 
 	for _, l := range logs {
 		c.cache[c.current] = l
-		c.lastlogidx = l.Index
+		c.lastLogIdx = l.Index
 		c.current = (c.current + 1) % len(c.cache)
 	}
 }
 
-func (c *LogCache) GetLog(logidx uint64, log *Log) error {
-	if cached, ok := c.getLogFromCache(logidx); ok {
+func (c *LogCache) GetLog(idx uint64, log *Log) error {
+	if cached, ok := c.getLogFromCache(idx); ok {
 		*log = *cached
 		return nil
 	}
-	return c.store.GetLog(logidx, log)
+	return c.store.GetLog(idx, log)
 }
 
 func (c *LogCache) StoreLog(log *Log) error {
@@ -83,7 +89,8 @@ func (c *LogCache) DeleteRange(min, max uint64) error {
 	c.l.Lock()
 	defer c.l.Unlock()
 
-	c.lastlogidx = 0
+	// Invalidate the cache on deletes
+	c.lastLogIdx = 0
 	c.current = 0
 	c.cache = make([]*Log, len(c.cache))
 
