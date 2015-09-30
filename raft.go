537bdb955279a52d2fdd66abc2db226b3728a49a
Sean Chittenden <seanc@groupon.com>
2015-09-30 09:16:16 -0700

    Remove overly defensive check
    If peers.json is missing a peer, but a peer's data is contained in the raft log, it is impossible to stop replicating to a peer that has been removed (i.e. recovering from a split brain or intentionally splitting a cluster).
    
    Reported by: @sean-
    Identified by: @armon

--- raft.go
+++ raft.go
@@ -1173,14 +1173,12 @@ func (r *Raft) processLog(l *Log, future *logFuture, precommit bool) {
 		if r.getState() == Leader && !precommit {
 			var toDelete []string
 			for _, repl := range r.leaderState.replState {
-				if !PeerContained(r.peers, repl.peer) {
 					r.logger.Printf("[INFO] raft: Removed peer %v, stopping replication (Index: %d)", repl.peer, l.Index)
 
 					// Replicate up to this index and stop
 					repl.stopCh <- l.Index
 					close(repl.stopCh)
 					toDelete = append(toDelete, repl.peer)
-				}
 			}
 			for _, name := range toDelete {
 				delete(r.leaderState.replState, name)
