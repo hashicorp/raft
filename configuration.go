5e26534d5474c6413fc9ec61d85977d65819c462
Brian Kassouf <briankassouf@users.noreply.github.com>
2019-06-17 11:15:16 -0700

    Update configuration.go

--- configuration.go
+++ configuration.go
@@ -33,7 +33,11 @@ func (s ServerSuffrage) String() string {
 }
 
 // ConfigurationStore provides an interface that can optionally be implemented by FSMs
-// to store configuration updates made in the replicated log.
+// to store configuration updates made in the replicated log. In general this is only 
+// necessary for FSMs that mutate durable state directly instead of applying changes 
+// in memory and snapshotting periodically. By storing configuration changes, the 
+// persistent FSM state can behave as a complete snapshot, and be able to recover 
+// without an external snapshot just for persisting the raft configuration.
 type ConfigurationStore interface {
 	// StoreConfiguration is invoked once a log entry containing a configuration
 	// change is committed. It takes the index at which the configuration was
