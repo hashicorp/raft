387ddaee31117cd6d27bb5695d9bc2a49f0632a3
Mahmood Ali <mahmood@hashicorp.com>
2020-03-03 14:44:05 -0500

    testing: increase timeouts
    Increase timeouts for TestRaft_UserRestore, as it makes file io calls.

--- inmem_transport.go
+++ inmem_transport.go
@@ -63,7 +63,7 @@ func NewInmemTransportWithTimeout(addr ServerAddress, timeout time.Duration) (Se
 // NewInmemTransport is used to initialize a new transport
 // and generates a random local address if none is specified
 func NewInmemTransport(addr ServerAddress) (ServerAddress, *InmemTransport) {
-	return NewInmemTransportWithTimeout(addr, 50*time.Millisecond)
+	return NewInmemTransportWithTimeout(addr, 500*time.Millisecond)
 }
 
 // SetHeartbeatHandler is used to set optional fast-path for
