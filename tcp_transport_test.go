ae3f4f2e0ae5e88dab8c9f6dc2521a45121f9a2a
Zhang Xiawei <shou1dwe@users.noreply.github.com>
2020-06-29 21:07:26 +0800

    Fail starting a tcp_transport with no ip (#403)

--- tcp_transport_test.go
+++ tcp_transport_test.go
@@ -12,6 +12,13 @@ func TestTCPTransport_BadAddr(t *testing.T) {
 	}
 }
 
+func TestTCPTransport_EmptyAddr(t *testing.T) {
+	_, err := NewTCPTransportWithLogger(":0", nil, 1, 0, newTestLogger(t))
+	if err != errNotAdvertisable {
+		t.Fatalf("err: %v", err)
+	}
+}
+
 func TestTCPTransport_WithAdvertise(t *testing.T) {
 	addr := &net.TCPAddr{IP: []byte{127, 0, 0, 1}, Port: 12345}
 	trans, err := NewTCPTransportWithLogger("0.0.0.0:0", addr, 1, 0, newTestLogger(t))
