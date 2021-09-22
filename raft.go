b40a18c7f0df74486a8838fe4801e02900879a9d
Dhia Ayachi <dhia@hashicorp.com>
2021-09-21 23:58:26 -0400

    do not transition to `Candidate` state if not part of stable configuration

--- raft.go
+++ raft.go
@@ -213,9 +213,13 @@ func (r *Raft) runFollower() {
 					didWarn = true
 				}
 			} else {
-				r.logger.Warn("heartbeat timeout reached, starting election", "last-leader", lastLeader)
 				metrics.IncrCounter([]string{"raft", "transition", "heartbeat_timeout"}, 1)
-				r.setState(Candidate)
+				if hasVote(r.configurations.latest, r.localID) {
+					r.logger.Warn("heartbeat timeout reached, starting election", "last-leader", lastLeader)
+					r.setState(Candidate)
+				} else {
+					r.logger.Warn("heartbeat timeout reached, not part of stable configuration, aborting election")
+				}
 				return
 			}
 
