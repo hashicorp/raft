bb62335fc5d3902ad9f22b860a76118ee896dd89
Dhia Ayachi <dhia@hashicorp.com>
2023-06-09 10:43:10 -0400

    extract cluster package

--- raft-compat/rolling_upgrade_test.go
+++ raft-compat/rolling_upgrade_test.go
@@ -1,140 +1,34 @@
 package raft_compat
 
 import (
-	"fmt"
 	raftrs "github.com/dhiayachi/raft"
 	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft/compat/testcluster"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
 
-type raftUIT struct {
-	raft   *raft.Raft
-	trans  *raft.NetworkTransport
-	Config *raft.Config
-	Store  *raft.InmemStore
-	Snap   *raft.InmemSnapshotStore
-	id     raft.ServerID
-	fsm    *raft.MockFSM
-}
-
-func (r raftUIT) getLocalID() string {
-	return string(r.id)
-}
-
-func (r raftUIT) getLeaderID() string {
-	_, id := r.raft.LeaderWithID()
-	return string(id)
-}
-
-type raftLatest struct {
-	raft   *raftrs.Raft
-	trans  *raftrs.NetworkTransport
-	Config *raftrs.Config
-	Store  *raftrs.InmemStore
-	Snap   *raftrs.InmemSnapshotStore
-	id     raftrs.ServerID
-	fsm    *raftrs.MockFSM
-}
-
-func (r raftLatest) getLocalID() string {
-	return string(r.id)
-}
-
-func (r raftLatest) getLeaderID() string {
-	_, id := r.raft.LeaderWithID()
-	return string(id)
-}
-
-type raftNode interface {
-	getLocalID() string
-	getLeaderID() string
-}
-
-type raftCluster[T raftNode] struct {
-	rafts []T
-}
-
-func newRaftCluster[T raftNode](count int, name string) raftCluster[T] {
-	rc := raftCluster[T]{}
-	rc.rafts = make([]T, count)
-	for i := 0; i < count; i++ {
-		initNode(&rc.rafts[i], fmt.Sprintf("%s-%d", name, i))
-	}
-	return rc
-}
-
-func (r *raftCluster[T]) getLeader() T {
-	var empty T
-	for _, n := range r.rafts {
-		if n.getLocalID() == n.getLeaderID() {
-			return n
-		}
-	}
-	return empty
-}
-
-func initNode(node interface{}, id string) {
-	switch node.(type) {
-	case *raftLatest:
-		initLatest(node.(*raftLatest), id)
-	case *raftUIT:
-		initUIT(node.(*raftUIT), id)
-	default:
-		panic("invalid node type")
-	}
-}
-
-func initUIT(node *raftUIT, id string) {
-	node.Config = raft.DefaultConfig()
-	node.Config.HeartbeatTimeout = 50 * time.Millisecond
-	node.Config.ElectionTimeout = 50 * time.Millisecond
-	node.Config.LeaderLeaseTimeout = 50 * time.Millisecond
-	node.Config.CommitTimeout = 5 * time.Millisecond
-	node.id = raft.ServerID(id)
-	node.Config.LocalID = node.id
-	node.Store = raft.NewInmemStore()
-	node.Snap = raft.NewInmemSnapshotStore()
-	node.fsm = &raft.MockFSM{}
-}
-
-func initLatest(node *raftLatest, id string) {
-	node.Config = raftrs.DefaultConfig()
-	node.Config.HeartbeatTimeout = 50 * time.Millisecond
-	node.Config.ElectionTimeout = 50 * time.Millisecond
-	node.Config.LeaderLeaseTimeout = 50 * time.Millisecond
-	node.Config.CommitTimeout = 5 * time.Millisecond
-	node.id = raftrs.ServerID(id)
-	node.Config.LocalID = node.id
-	node.Store = raftrs.NewInmemStore()
-	node.Snap = raftrs.NewInmemSnapshotStore()
-	node.fsm = &raftrs.MockFSM{}
-}
-
 func TestRaft_RollingUpgrade(t *testing.T) {
 
 	initCount := 3
-	rLatest := newRaftCluster[raftLatest](initCount, "raftOld")
+	rLatest := testcluster.NewRaftCluster[testcluster.RaftLatest](t, initCount, "raftOld")
 	configuration := raftrs.Configuration{}
 
 	for i := 0; i < initCount; i++ {
 		var err error
-		rLatest.rafts[i].trans, err = raftrs.NewTCPTransport("localhost:0", nil, 2, time.Second, nil)
-		require.NoError(t, err)
-		rLatest.rafts[i].raft, err = raftrs.NewRaft(rLatest.rafts[i].Config, rLatest.rafts[i].fsm, rLatest.rafts[i].Store,
-			rLatest.rafts[i].Store, rLatest.rafts[i].Snap, rLatest.rafts[i].trans)
 		require.NoError(t, err)
 		configuration.Servers = append(configuration.Servers, raftrs.Server{
-			ID:      rLatest.rafts[i].id,
-			Address: rLatest.rafts[i].trans.LocalAddr(),
+			ID:      raftrs.ServerID(rLatest.ID(i)),
+			Address: raftrs.ServerAddress(rLatest.Addr(i)),
 		})
 	}
-	boot := rLatest.rafts[0].raft.BootstrapCluster(configuration)
+	raft0 := rLatest.Raft(0).(*raftrs.Raft)
+	boot := raft0.BootstrapCluster(configuration)
 	if err := boot.Error(); err != nil {
 		t.Fatalf("bootstrap err: %v", err)
 	}
-	lCh := rLatest.rafts[0].raft.LeaderCh()
+	lCh := raft0.LeaderCh()
 
 	after := time.After(5 * time.Second)
 
@@ -143,58 +37,52 @@ func TestRaft_RollingUpgrade(t *testing.T) {
 		t.Fatalf("timedout")
 	case <-lCh:
 	}
-	getLeader := rLatest.getLeader()
+	getLeader := rLatest.GetLeader()
 	require.NotNil(t, getLeader)
-	a, _ := getLeader.raft.LeaderWithID()
+	a, _ := getLeader.GetRaft().(*raftrs.Raft).LeaderWithID()
 	require.NotEmpty(t, a)
-	future := getLeader.raft.Apply([]byte("test"), time.Second)
+	future := getLeader.GetRaft().(*raftrs.Raft).Apply([]byte("test"), time.Second)
 	require.NoError(t, future.Error())
 
-	rUIT := newRaftCluster[raftUIT](initCount, "raftNew")
-	leader, _ := getLeader.raft.LeaderWithID()
+	rUIT := testcluster.NewRaftCluster[testcluster.RaftUIT](t, initCount, "raftNew")
+	leader, _ := getLeader.GetRaft().(*raftrs.Raft).LeaderWithID()
 	require.NotEmpty(t, leader)
 
 	// Upgrade all the followers
 	leaderIdx := 0
-	for i := 0; i < len(rLatest.rafts); i++ {
-		if getLeader.getLocalID() == rLatest.rafts[i].getLocalID() {
+	for i := 0; i < initCount; i++ {
+		if getLeader.GetLocalID() == rLatest.ID(i) {
 			leaderIdx = i
 			continue
 		}
-		var err error
-		rUIT.rafts[i].trans, err = raft.NewTCPTransport("localhost:0", nil, 2, time.Second, nil)
-		require.NoError(t, err)
-		rUIT.rafts[i].raft, err = raft.NewRaft(rUIT.rafts[i].Config, rUIT.rafts[i].fsm, rUIT.rafts[i].Store,
-			rUIT.rafts[i].Store, rUIT.rafts[i].Snap, rUIT.rafts[i].trans)
-		getLeader.raft.AddVoter(raftrs.ServerID(rUIT.rafts[i].getLocalID()), raftrs.ServerAddress(rUIT.rafts[i].trans.LocalAddr()), 0, 0)
+
+		future := getLeader.GetRaft().(*raftrs.Raft).AddVoter(raftrs.ServerID(rUIT.ID(i)), raftrs.ServerAddress(rUIT.Addr(i)), 0, 0)
 
 		time.Sleep(1 * time.Second)
-
+		require.NoError(t, future.Error())
 		// Check Leader haven't changed as we are not replacing the leader
-		a, _ := getLeader.raft.LeaderWithID()
+		a, _ := getLeader.GetRaft().(*raftrs.Raft).LeaderWithID()
 		require.Equal(t, a, leader)
-		getLeader.raft.RemoveServer(rLatest.rafts[i].id, 0, 0)
-		rLatest.rafts[i].raft.Shutdown()
+		getLeader.GetRaft().(*raftrs.Raft).RemoveServer(raftrs.ServerID(rLatest.ID(i)), 0, 0)
+		rLatest.Raft(i).(*raftrs.Raft).Shutdown()
 	}
-	future = getLeader.raft.Apply([]byte("test2"), time.Second)
+	future = getLeader.GetRaft().(*raftrs.Raft).Apply([]byte("test2"), time.Second)
 	require.NoError(t, future.Error())
 
-	var err error
-	rUIT.rafts[leaderIdx].trans, err = raft.NewTCPTransport("localhost:0", nil, 2, time.Second, nil)
-	require.NoError(t, err)
-	rUIT.rafts[leaderIdx].raft, err = raft.NewRaft(rUIT.rafts[leaderIdx].Config, rUIT.rafts[leaderIdx].fsm, rUIT.rafts[leaderIdx].Store,
-		rUIT.rafts[leaderIdx].Store, rUIT.rafts[leaderIdx].Snap, rUIT.rafts[leaderIdx].trans)
-	getLeader.raft.AddVoter(raftrs.ServerID(rUIT.rafts[leaderIdx].getLocalID()), raftrs.ServerAddress(rUIT.rafts[leaderIdx].trans.LocalAddr()), 0, 0)
+	f := getLeader.GetRaft().(*raftrs.Raft).AddVoter(raftrs.ServerID(rUIT.ID(leaderIdx)), raftrs.ServerAddress(rUIT.Addr(leaderIdx)), 0, 0)
+	time.Sleep(1 * time.Second)
+	require.NoError(t, f.Error())
 	// Check Leader haven't changed as we are not replacing the leader
-	a, _ = getLeader.raft.LeaderWithID()
+	a, _ = getLeader.GetRaft().(*raftrs.Raft).LeaderWithID()
 	require.Equal(t, a, leader)
-	getLeader.raft.RemoveServer(rLatest.rafts[leaderIdx].id, 0, 0)
+	getLeader.GetRaft().(*raftrs.Raft).RemoveServer(raftrs.ServerID(rLatest.ID(leaderIdx)), 0, 0)
 	time.Sleep(1 * time.Second)
-	rLatest.rafts[leaderIdx].raft.Shutdown()
+	rLatest.Raft(leaderIdx).(*raftrs.Raft).Shutdown()
 	time.Sleep(1 * time.Second)
-	aNew, _ := getLeader.raft.LeaderWithID()
+	newLeader := rUIT.GetLeader()
+	aNew, _ := newLeader.GetRaft().(*raft.Raft).LeaderWithID()
 	require.NotEqual(t, aNew, leader)
 
-	require.Len(t, rUIT.getLeader().fsm.Logs(), 2)
+	require.Equal(t, newLeader.NumLogs(), 2)
 
 }
--- raft-compat/testcluster/cluster.go
+++ raft-compat/testcluster/cluster.go
@@ -2,8 +2,8 @@ package testcluster
 
 import (
 	"fmt"
+	raftrs "github.com/dhiayachi/raft"
 	"github.com/hashicorp/raft"
-	raftprevious "github.com/hashicorp/raft-previous-version"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
@@ -31,10 +31,6 @@ func (r RaftUIT) GetRaft() interface{} {
 	return r.raft
 }
 
-func (r RaftUIT) GetStore() interface{} {
-	return r.Store
-}
-
 func (r RaftUIT) GetLocalID() string {
 	return string(r.id)
 }
@@ -44,31 +40,25 @@ func (r RaftUIT) GetLeaderID() string {
 	return string(id)
 }
 
-func (r *RaftCluster) ID(i int) string {
+func (r *raftCluster[T]) ID(i int) string {
 	return r.rafts[i].GetLocalID()
 }
-func (r *RaftCluster) Addr(i int) string {
+func (r *raftCluster[T]) Addr(i int) string {
 	return r.rafts[i].GetLocalAddr()
 }
 
-func (r *RaftCluster) Raft(id string) interface{} {
-	i := r.GetIndex(id)
+func (r *raftCluster[T]) Raft(i int) interface{} {
 	return r.rafts[i].GetRaft()
 }
 
-func (r *RaftCluster) Store(id string) interface{} {
-	i := r.GetIndex(id)
-	return r.rafts[i].GetStore()
-}
-
 type RaftLatest struct {
-	raft   *raftprevious.Raft
-	trans  *raftprevious.NetworkTransport
-	Config *raftprevious.Config
-	Store  *raftprevious.InmemStore
-	Snap   *raftprevious.InmemSnapshotStore
-	id     raftprevious.ServerID
-	fsm    *raftprevious.MockFSM
+	raft   *raftrs.Raft
+	trans  *raftrs.NetworkTransport
+	Config *raftrs.Config
+	Store  *raftrs.InmemStore
+	Snap   *raftrs.InmemSnapshotStore
+	id     raftrs.ServerID
+	fsm    *raftrs.MockFSM
 }
 
 func (r RaftLatest) NumLogs() int {
@@ -81,9 +71,6 @@ func (r RaftLatest) GetLocalAddr() string {
 
 func (r RaftLatest) GetRaft() interface{} {
 	return r.raft
-}
-func (r RaftLatest) GetStore() interface{} {
-	return r.Store
 }
 
 func (r RaftLatest) GetLocalID() string {
@@ -100,62 +87,44 @@ type RaftNode interface {
 	GetLocalAddr() string
 	GetLeaderID() string
 	GetRaft() interface{}
-	GetStore() interface{}
 	NumLogs() int
 }
 
-type RaftCluster struct {
-	rafts []RaftNode
+type raftCluster[T RaftNode] struct {
+	rafts []T
 }
 
-func NewRaftCluster(t *testing.T, f func(t *testing.T, id string) RaftNode, count int, name string) RaftCluster {
-	rc := RaftCluster{}
-	rc.rafts = make([]RaftNode, count)
+func NewRaftCluster[T RaftNode](t *testing.T, count int, name string) raftCluster[T] {
+	rc := raftCluster[T]{}
+	rc.rafts = make([]T, count)
 	for i := 0; i < count; i++ {
-		rc.rafts[i] = f(t, fmt.Sprintf("%s-%d", name, i))
+		initNode(t, &rc.rafts[i], fmt.Sprintf("%s-%d", name, i))
 	}
 	return rc
 }
 
-func (r *RaftCluster) GetLeader() RaftNode {
+func (r *raftCluster[T]) GetLeader() T {
+	var empty T
 	for _, n := range r.rafts {
 		if n.GetLocalID() == n.GetLeaderID() {
 			return n
 		}
 	}
-	return nil
+	return empty
 }
 
-func (r *RaftCluster) Len() int {
-	return len(r.rafts)
-}
-
-func (r *RaftCluster) AddNode(node RaftNode) {
-	r.rafts = append([]RaftNode{node}, r.rafts...)
-}
-
-func (r *RaftCluster) DeleteNode(id string) {
-	i := r.GetIndex(id)
-	r.rafts = append(r.rafts[:i], r.rafts[i+1:]...)
-}
-
-func (r *RaftCluster) GetIndex(id string) int {
-	i := 0
-	for _, r := range r.rafts {
-		if r.GetLocalID() == id {
-			return i
-		}
-		i++
+func initNode(t *testing.T, node interface{}, id string) {
+	switch node.(type) {
+	case *RaftLatest:
+		initLatest(t, node.(*RaftLatest), id)
+	case *RaftUIT:
+		initUIT(t, node.(*RaftUIT), id)
+	default:
+		panic("invalid node type")
 	}
-	return -1
 }
 
-func InitUIT(t *testing.T, id string) RaftNode {
-	return InitUITWithStore(t, id, nil)
-}
-
-func InitUITWithStore(t *testing.T, id string, store *raftprevious.InmemStore) RaftNode {
-	node := RaftUIT{}
+func initUIT(t *testing.T, node *RaftUIT, id string) {
 	node.Config = raft.DefaultConfig()
 	node.Config.HeartbeatTimeout = 50 * time.Millisecond
 	node.Config.ElectionTimeout = 50 * time.Millisecond
@@ -163,12 +132,7 @@ func InitUITWithStore(t *testing.T, id string, store *raftprevious.InmemStore) R
 	node.Config.CommitTimeout = 5 * time.Millisecond
 	node.id = raft.ServerID(id)
 	node.Config.LocalID = node.id
-	if store != nil {
-		node.Store = convertInMemStore(store)
-	} else {
-		node.Store = raft.NewInmemStore()
-	}
-
+	node.Store = raft.NewInmemStore()
 	node.Snap = raft.NewInmemSnapshotStore()
 	node.fsm = &raft.MockFSM{}
 	var err error
@@ -177,75 +141,23 @@ func InitUITWithStore(t *testing.T, id string, store *raftprevious.InmemStore) R
 	node.raft, err = raft.NewRaft(node.Config, node.fsm, node.Store,
 		node.Store, node.Snap, node.trans)
 	require.NoError(t, err)
-	return node
 }
 
-func InitLatest(t *testing.T, id string) RaftNode {
-	node := RaftLatest{}
-	node.Config = raftprevious.DefaultConfig()
+func initLatest(t *testing.T, node *RaftLatest, id string) {
+	node.Config = raftrs.DefaultConfig()
 	node.Config.HeartbeatTimeout = 50 * time.Millisecond
 	node.Config.ElectionTimeout = 50 * time.Millisecond
 	node.Config.LeaderLeaseTimeout = 50 * time.Millisecond
 	node.Config.CommitTimeout = 5 * time.Millisecond
-	node.id = raftprevious.ServerID(id)
+	node.id = raftrs.ServerID(id)
 	node.Config.LocalID = node.id
-
-	node.Store = raftprevious.NewInmemStore()
-	node.Snap = raftprevious.NewInmemSnapshotStore()
-	node.fsm = &raftprevious.MockFSM{}
+	node.Store = raftrs.NewInmemStore()
+	node.Snap = raftrs.NewInmemSnapshotStore()
+	node.fsm = &raftrs.MockFSM{}
 	var err error
-	node.trans, err = raftprevious.NewTCPTransport("localhost:0", nil, 2, time.Second, nil)
+	node.trans, err = raftrs.NewTCPTransport("localhost:0", nil, 2, time.Second, nil)
 	require.NoError(t, err)
-	node.raft, err = raftprevious.NewRaft(node.Config, node.fsm, node.Store,
+	node.raft, err = raftrs.NewRaft(node.Config, node.fsm, node.Store,
 		node.Store, node.Snap, node.trans)
 	require.NoError(t, err)
-	return node
-}
-
-func convertLog(ll *raftprevious.Log) *raft.Log {
-	l := new(raft.Log)
-	l.Index = ll.Index
-	l.AppendedAt = ll.AppendedAt
-	l.Type = raft.LogType(ll.Type)
-	l.Term = ll.Term
-	l.Data = ll.Data
-	l.Extensions = ll.Extensions
-	return l
-}
-
-var (
-	keyCurrentTerm  = []byte("CurrentTerm")
-	keyLastVoteTerm = []byte("LastVoteTerm")
-	keyLastVoteCand = []byte("LastVoteCand")
-)
-
-func convertInMemStore(s *raftprevious.InmemStore) *raft.InmemStore {
-	ss := raft.NewInmemStore()
-	fi, _ := s.FirstIndex()
-	li, _ := s.LastIndex()
-	for i := fi; i <= li; i++ {
-		log := new(raftprevious.Log)
-		s.GetLog(i, log)
-		ss.StoreLog(convertLog(log))
-	}
-
-	get, _ := ss.Get(keyCurrentTerm)
-	ss.Set(keyCurrentTerm, get)
-
-	get, _ = ss.Get(keyLastVoteTerm)
-	ss.Set(keyLastVoteTerm, get)
-
-	get, _ = ss.Get(keyLastVoteCand)
-	ss.Set(keyLastVoteCand, get)
-
-	get64, _ := ss.GetUint64(keyCurrentTerm)
-	ss.SetUint64(keyCurrentTerm, get64)
-
-	get64, _ = ss.GetUint64(keyLastVoteTerm)
-	ss.SetUint64(keyLastVoteTerm, get64)
-
-	get64, _ = ss.GetUint64(keyLastVoteCand)
-	ss.SetUint64(keyLastVoteCand, get64)
-
-	return ss
 }
