20e8701025259190bd96627d6ecfc91792f6a675
peterxcli <peterxcli@gmail.com>
2024-09-19 10:01:45 +0800

    refactor(inmem-commit-tracking-store): simplify in-mem log tracking store implement

--- inmem_store.go
+++ inmem_store.go
@@ -6,6 +6,7 @@ package raft
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 // InmemStore implements the LogStore and StableStore interface.
@@ -137,142 +138,24 @@ type commitIndexTrackingLog struct {
 	CommitIndex uint64
 }
 type InmemCommitTrackingStore struct {
-	l         sync.RWMutex
-	lowIndex  uint64
-	highIndex uint64
-	logs      map[uint64]*commitIndexTrackingLog
-	kv        map[string][]byte
-	kvInt     map[string]uint64
-
-	commitIndexLock sync.RWMutex
-	commitIndex     uint64
+	InmemStore
+	commitIndex atomic.Uint64
 }
 
 // NewInmemCommitTrackingStore returns a new in-memory backend that tracks the commit index. Do not ever
 // use for production. Only for testing.
 func NewInmemCommitTrackingStore() *InmemCommitTrackingStore {
 	i := &InmemCommitTrackingStore{
-		logs:  make(map[uint64]*commitIndexTrackingLog),
-		kv:    make(map[string][]byte),
-		kvInt: make(map[string]uint64),
+		InmemStore: *NewInmemStore(),
 	}
 	return i
 }
 
-// FirstIndex implements the CommitTrackingLogStore interface.
-func (i *InmemCommitTrackingStore) FirstIndex() (uint64, error) {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	return i.lowIndex, nil
-}
-
-// LastIndex implements the CommitTrackingLogStore interface.
-func (i *InmemCommitTrackingStore) LastIndex() (uint64, error) {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	return i.highIndex, nil
-}
-
-// GetLog implements the CommitTrackingLogStore interface.
-func (i *InmemCommitTrackingStore) GetLog(index uint64, log *Log) error {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	l, ok := i.logs[index]
-	if !ok {
-		return ErrLogNotFound
-	}
-	*log = *l.log
-	return nil
-}
-
-// StoreLog implements the LogStore interface.
-func (i *InmemCommitTrackingStore) StoreLog(log *Log) error {
-	return i.StoreLogs([]*Log{log})
-}
-
-// StoreLogs implements the LogStore interface.
-func (i *InmemCommitTrackingStore) StoreLogs(logs []*Log) error {
-	i.l.Lock()
-	defer i.l.Unlock()
-	for _, l := range logs {
-		i.logs[l.Index] = &commitIndexTrackingLog{log: l, CommitIndex: i.commitIndex}
-		if i.lowIndex == 0 {
-			i.lowIndex = l.Index
-		}
-		if l.Index > i.highIndex {
-			i.highIndex = l.Index
-		}
-	}
-	return nil
-}
-
-// DeleteRange implements the LogStore interface.
-func (i *InmemCommitTrackingStore) DeleteRange(min, max uint64) error {
-	i.l.Lock()
-	defer i.l.Unlock()
-	for j := min; j <= max; j++ {
-		delete(i.logs, j)
-	}
-	if min <= i.lowIndex {
-		i.lowIndex = max + 1
-	}
-	if max >= i.highIndex {
-		i.highIndex = min - 1
-	}
-	if i.lowIndex > i.highIndex {
-		i.lowIndex = 0
-		i.highIndex = 0
-	}
-	return nil
-}
-
-// Set implements the StableStore interface.
-func (i *InmemCommitTrackingStore) Set(key []byte, val []byte) error {
-	i.l.Lock()
-	defer i.l.Unlock()
-	i.kv[string(key)] = val
-	return nil
-}
-
-// Get implements the StableStore interface.
-func (i *InmemCommitTrackingStore) Get(key []byte) ([]byte, error) {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	val := i.kv[string(key)]
-	if val == nil {
-		return nil, errors.New("not found")
-	}
-	return val, nil
-}
-
-// SetUint64 implements the StableStore interface.
-func (i *InmemCommitTrackingStore) SetUint64(key []byte, val uint64) error {
-	i.l.Lock()
-	defer i.l.Unlock()
-	i.kvInt[string(key)] = val
-	return nil
-}
-
-// GetUint64 implements the StableStore interface.
-func (i *InmemCommitTrackingStore) GetUint64(key []byte) (uint64, error) {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	return i.kvInt[string(key)], nil
-}
-
 func (i *InmemCommitTrackingStore) SetCommitIndex(index uint64) error {
-	i.commitIndexLock.Lock()
-	defer i.commitIndexLock.Unlock()
-	i.commitIndex = index
+	i.commitIndex.Store(index)
 	return nil
 }
 
 func (i *InmemCommitTrackingStore) GetCommitIndex() (uint64, error) {
-	i.l.RLock()
-	defer i.l.RUnlock()
-	log, ok := i.logs[i.highIndex]
-	if !ok {
-		return 0, ErrLogNotFound
-	}
-	return log.CommitIndex, nil
+	return i.commitIndex.Load(), nil
 }
